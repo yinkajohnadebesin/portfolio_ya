fd44c2b80a04a9905a54efc76e4ea8c8ddecf9a0
Yinka Adebesin <yinkaadebesin@gmail.com>
2025-07-03 06:39:20 +0100

    testing auto redeploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl font-bold">
-      Portfolio Frontend Ready 🚀
+      Coming soon... 🚀
     </div>
   );
 }
